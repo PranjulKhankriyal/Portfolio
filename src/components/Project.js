b223a46bd5129c15f06e7f6675fc47bda9cf81cc
PranjulKhankriyal <shivamkhankriyal515@gmail.com>
2024-03-08 19:34:14 +0530

    add comment in project

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,8 @@ import React from "react";
 function Project(){
     return(
         <div className="project-body">
-            this is project page.!!!
+            this is project page.!!!<br/>
+            under construction
         </div>
     );
 }
